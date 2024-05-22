b2fda4be63b37250a48953ab729f651b5794b571
dinhtruongnguyen11 <cydeva.tech@gmail.com>
2024-05-22 15:13:54 +0700

    1

--- ESRGAN/upscale.py
+++ ESRGAN/upscale.py
@@ -12,7 +12,7 @@ import RRDBNet_arch as arch
 
 ORIGINAL_PATH = '/root/cts/ai01/data'
 
-IMG_SIZE = 768
+IMG_SIZE = 640
 
 def process_images_in_batch(paths, device, model, start_idx):
     for idx, path in enumerate(paths, start=start_idx):
